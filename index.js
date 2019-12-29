fecd885cd2d369ccde01f6d25ada5fc0f688b558
Kaur <skaur1@bechtel.com>
2019-12-29 13:51:44 +0530

    Initial release

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {num1} number
+ *  * @param {num2} number
+ * @return {number}
+
+ */
+
+ module.exports = function(num1, num2) {
+     return num1 + num2;
+ };
